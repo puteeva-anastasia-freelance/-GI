cc57b0a46459d6c09dbb190257bded2e60b0f3cb
Anastasia <anastasiaputeeya@gmail.com>
2022-09-13 17:08:44 +0300

    add banners

--- app/js/app.js
+++ app/js/app.js
@@ -31,9 +31,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	var bannersSwiper = new Swiper('#banners-slider', {
 		speed: 1200,
 		loop: true,
-		autoplay: {
+		/*autoplay: {
 			delay: 5000,
-		},
+		},*/
 		slidesPerView: 'auto',
 		autoHeight: true,
 		pagination: {
@@ -45,6 +45,14 @@ document.addEventListener('DOMContentLoaded', () => {
 			prevEl: '.banners__button-prev',
 		},
 	})
+
+	/*$("#banners-slider").mouseenter(function () {
+		bannersSwiper.autoplay.stop();
+	});
+
+	$("#banners-slider").mouseleave(function () {
+		bannersSwiper.autoplay.start();
+	});*/
 
 	var recSwiper = new Swiper('#rec-slider', {
 		loop: true,
